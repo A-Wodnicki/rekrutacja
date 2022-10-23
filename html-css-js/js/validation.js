1fff5644c0274ae6f0c33453835fb21406637c5b
A-Wodnicki <wodnicki.adrian4@gmail.com>
2022-10-23 23:39:28 +0200

    Rozwiazania do frontend i js

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -1,11 +1,45 @@
-/*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    AKAI Frontend Task - Javascript
+const f = document.querySelector("form");
 
-    W tym zadaniu postaraj się zaimplementować metody, które sprawdzą, czy dane wprowadzone
-    do formularza są poprawne. Przykładowo: czy imię i nazwisko zostało wprowadzone.
-    Możesz rozwinąć walidację danych tak bardzo, jak tylko zapragniesz.
+function isNameValid(name) {
+  const pattern = /^[A-Z][a-z]+$/;
+  return pattern.test(name);
+}
 
-    Powodzenia!
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
+f.addEventListener("submit", function (e) {
+  const checkboxesSection = document.querySelector(".sections-checkboxes");
+
+  document.querySelectorAll(".validation").forEach((e) => e.remove());
+
+  if (!(isNameValid(f.firstName.value) && isNameValid(f.lastName.value))) {
+    if (!isNameValid(f.firstName.value))
+      f.firstName.insertAdjacentHTML(
+        "afterend",
+        "<span class='validation'>Imię powinno składać się z jednej wielkiej litery i co najmniej jednej małej litery</span>"
+      );
+    else
+      f.lastName.insertAdjacentHTML(
+        "afterend",
+        "<span class='validation'>Nazwisko powinno składać się z jednej wielkiej litery i co najmnej jednej małej litery</span>"
+      );
+    e.preventDefault();
+    return;
+  }
+
+  if (f.email.value === "") {
+    f.email.insertAdjacentHTML(
+      "afterend",
+      "<span class='validation'>Proszę wypełnić to pole</span>"
+    );
+    e.preventDefault();
+    return;
+  }
+
+  if (!Array.prototype.some.call(f.section, (checkbox) => checkbox.checked)) {
+    checkboxesSection.insertAdjacentHTML(
+      "afterend",
+      "<span class='validation'>Proszę zaznaczyć przynajmniej jedną pozycję</span>"
+    );
+    e.preventDefault();
+    return;
+  }
+});
